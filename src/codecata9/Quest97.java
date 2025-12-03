5fda0940570fe8401f263375ad334ab0372e1b0b
mingang211 <a989977@naver.com>
2025-12-03 15:33:05 +0900

    모음 사전
    등비수열 계산식 추가

--- src/codecata9/Quest97.java
+++ src/codecata9/Quest97.java
@@ -13,7 +13,23 @@ public class Quest97 {
 class Solution97 {
     public int solution(String word) {
         //자리별 가중치 (등비수열의 합)
-        int[] weights = {781, 156, 31, 6, 1};
+        final int MAX_LENGTH = 5; // 단어 최대 길이
+        final int NUM_VOWELS = 5; // 모음 개수 (A, E, I, O, U)
+
+        // 등비수열의 합을 for문으로 계산하여 weights 배열 채우기
+        int[] weights = new int[MAX_LENGTH];
+
+        for (int i = 0; i < MAX_LENGTH; i++) {
+            int lengthToSum = MAX_LENGTH - i;
+            int currentWeight = 0;
+
+            // 5^0 부터 5^(lengthToSum - 1) 까지 합산
+            for (int j = 0; j < lengthToSum; j++) {
+                // Math.pow는 double을 반환하므로 int로 형변환
+                currentWeight += (int) Math.pow(NUM_VOWELS, j);
+            }
+            weights[i] = currentWeight;
+        }
 
         //모음 순서 매핑 (A=0, E=1, I=2, O=3, U=4)
         Map<Character, Integer> vowelIndex = new HashMap<>();
